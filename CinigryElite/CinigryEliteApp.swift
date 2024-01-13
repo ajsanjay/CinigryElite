6a3aaefacc3e5ec10bf0c8b84130cccd5556639d
Jayasabeen <aj.sabeen@gmail.com>
2024-01-13 12:01:20 +0530

    Created Empty project

---
+++ CinigryElite/CinigryEliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinigryEliteApp.swift
+//  CinigryElite
+//
+//  Created by jay sabeen on 13/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CinigryEliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
